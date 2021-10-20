5ecda569b888281318640d99428ddf5c00fe7d4e
Phillip Miller <pmiller@human-element.com>
2021-10-20 01:06:17 -0400

    Added main_test.go to improve coverage score

---
+++ main_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"pimp-my-shell/localio"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func Test_pimpMyShell(t *testing.T) {
+	dirs, err := localio.NewDirectories()
+	if err != nil {
+		t.Errorf("failed to create Directories type: %v", err)
+	}
+	type args struct {
+		osType            string
+		dirs              *localio.Directories
+		installedPackages *localio.InstalledPackages
+	}
+	packages := &localio.InstalledPackages{}
+	packages.BrewInstalledPackages = &localio.BrewInstalled{
+		Names: []string{"cointop"}, CaskFullNames: []string{"cointop"}, Taps: []string{"homebrew/core"},
+	}
+	packages.AptInstalledPackages = &localio.AptInstalled{Name: []string{"sslscan"}}
+	osType := runtime.GOOS
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: fmt.Sprintf("Test Test_pimpMyShell %s 1", osType), args: args{
+			osType:            osType,
+			dirs:              dirs,
+			installedPackages: packages,
+		}},
+	}
+	timeout := time.After(30 * time.Minute)
+	done := make(chan bool)
+	go func() {
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				if err := pimpMyShell(tt.args.osType, tt.args.dirs, tt.args.installedPackages); (err != nil) != tt.wantErr {
+					t.Errorf("pimpMyShell() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			})
+		}
+		done <- true
+	}()
+	select {
+	case <-timeout:
+		t.Fatal("Test didn't finish in time")
+	case <-done:
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{name: "Test_main 1"},
+	}
+	timeout := time.After(30 * time.Minute)
+	done := make(chan bool)
+	go func() {
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				main()
+			})
+		}
+		done <- true
+	}()
+	select {
+	case <-timeout:
+		t.Fatal("Test didn't finish in time")
+	case <-done:
+	}
+}
